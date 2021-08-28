2f78c1138300529b00c5fab930d546deab285527
gammaparticle <575507+gammaparticle@users.noreply.github.com>
2021-08-28 15:56:40 -0400

    Remove references to publishing single file and references to DirectXTexSharp nuget. Other minor modifications added inadvertently - will remove these, if they're further not needed.

--- DirectXTexSharpApp/TestApp.cs
+++ DirectXTexSharpApp/TestApp.cs
@@ -13,18 +13,15 @@ namespace DirectXTexSharpApp
         {
             Array.Clear(_buffer, 0, _buffer.Length);
             _buffer = File.ReadAllBytes(ddsFilePath);
-            
 
             fixed (byte* ptr = _buffer)
             {
                 var ddsFileOutDir = Path.Combine(new FileInfo(ddsFilePath).Directory.FullName, "out");
                 Directory.CreateDirectory(ddsFileOutDir);
-                                                
                 var fileName = Path.GetFileNameWithoutExtension(ddsFilePath);
                 var saveFileExtension = filetype.ToString().ToLower();
                 var saveFilePath = Path.Combine(ddsFileOutDir, $"{fileName}.{saveFileExtension}");
                 var len = _buffer.Length;
-                
                 // test direct saving
                 DirectXTexSharp.Texconv.ConvertAndSaveDdsImage(ptr, len, saveFilePath, filetype, false, false);
 
@@ -35,18 +32,19 @@ namespace DirectXTexSharpApp
                 File.WriteAllBytes(newSaveFilePath, mbuffer);
             }
 
+
         }
-        static void Main(string[] args)
+        public static void Main()
         {
             string ddsFilePath = @"D:\CyberPunkAssets\chicken_brown_d.dds";
             TestApp TexClient = new();
             foreach (int ft in Enum.GetValues(typeof(ESaveFileTypes)))
-            {                
+            {
                 Console.WriteLine("File type is: {0}", (ESaveFileTypes) ft);
                 ESaveFileTypes saveFiletype = (ESaveFileTypes) ft;
                 DXGI_FORMAT_WRAPPED dxgiFormat = DXGI_FORMAT_WRAPPED.DXGI_FORMAT_UNKNOWN;
-                TexClient.TCConvertAndSaveDdsImage(ddsFilePath, saveFiletype, dxgiFormat);               
-            }                 
+                TexClient.TCConvertAndSaveDdsImage(ddsFilePath, saveFiletype, dxgiFormat);
+            }
         }
     }
 }
